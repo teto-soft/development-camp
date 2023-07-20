465d433c06c633e32b7208ea672fd3e03941e1eb
katsuki_maemura <maemura.katsuki@suzukishouten.co.jp>
2023-07-21 01:20:11 +0900

    feat: first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/aT3V5qfQmAQBIEPRpZHQMg/**',
+      },
+    ],
+  },
+};
