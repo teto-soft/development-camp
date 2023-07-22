fe68a70cb8d669fcee46f24bba81ebc2535fbdf9
teto-soft <48591301+teto-soft@users.noreply.github.com>
2023-07-22 18:06:48 +0900

    seed 値を登録

---
+++ supabase/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO todos (id, created_at, user_id, todo, is_completed)
+VALUES (1, '2021-10-13 00:00:00', '1', 'Buy milk', false),
+       (2, '2021-10-13 00:00:00', '1', 'Walk the dog', false),
+       (3, '2021-10-13 00:00:00', '2', 'Buy milk', false),
+       (4, '2021-10-13 00:00:00', '2', 'Walk the dog', false);
