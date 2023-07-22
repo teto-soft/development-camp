c4e2dcc8f10e3d2eb183693f2b475d5d3e3f1ddd
tsuyoshi <tsuyoshi.t201603@gmail.com>
2023-07-22 23:04:22 +0900

    ログイン時のルーティング設定

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,18 @@
+import { useRouter } from 'next/router';
+import { supabase } from 'pages/_app';
+
 import LoginPage from '@/components/page/login/login.page';
 
-const Login = () => <LoginPage />;
+const Login = () => {
+  const router = useRouter();
+
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (session?.user) {
+      router.push('/');
+    }
+  });
+
+  return <LoginPage />;
+};
 
 export default Login;
