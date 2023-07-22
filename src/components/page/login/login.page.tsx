1220eb82ff3762fe6d63552711a7aee197431540
tsuyoshi <tsuyoshi.t201603@gmail.com>
2023-07-23 02:03:27 +0900

    ログイン画面のレイアウト修正

--- src/components/page/login/login.page.tsx
+++ src/components/page/login/login.page.tsx
@@ -1,13 +1,16 @@
+import { Box } from '@chakra-ui/react';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { supabase } from 'pages/_app';
 
 const LoginPage = () => (
-  <Auth
-    appearance={{ theme: ThemeSupa }}
-    providers={['google', 'twitter']}
-    supabaseClient={supabase}
-  />
+  <Box mx="35%" pt="10%">
+    <Auth
+      appearance={{ theme: ThemeSupa }}
+      providers={['google', 'twitter']}
+      supabaseClient={supabase}
+    />
+  </Box>
 );
 
 export default LoginPage;
