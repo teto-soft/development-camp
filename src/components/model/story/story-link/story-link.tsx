92aa17d120cdb6406f9b96c284166bcc0347ab97
teto-soft <48591301+teto-soft@users.noreply.github.com>
2023-07-23 13:03:01 +0900

    story-link.tsx を修正

--- src/components/model/story/story-link/story-link.tsx
+++ src/components/model/story/story-link/story-link.tsx
@@ -2,13 +2,12 @@ import { ChatIcon } from '@chakra-ui/icons';
 import { Button, Text } from '@chakra-ui/react';
 
 interface StoryLinkProps {
-  key: number;
   name: string;
 }
 
-export default function StoryLink({ key, name }: StoryLinkProps) {
+export default function StoryLink({ name }: StoryLinkProps) {
   return (
-    <Button _hover={{ bg: 'white' }} key={key} w="190px">
+    <Button _hover={{ bg: 'white' }} w="190px">
       <ChatIcon mr="2" />
       <Text>{name}</Text>
     </Button>
