98667f3cc55c05028889eeb42955c15337824fec
tsuyoshi <tsuyoshi.t201603@gmail.com>
2023-07-23 16:19:00 +0900

    ログイン、ログイン後の画面遷移を修正

--- middleware.ts
+++ middleware.ts
@@ -2,32 +2,31 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-import { TableNames } from '@/constants/table-and-view-names';
+import { TableNames } from '@/constants';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   const supabase = createMiddlewareClient({ req, res });
+
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  if (!session) {
-    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-  }
+  if (session) {
+    const email = session?.user?.email;
+    const { data } = await supabase
+      .from(TableNames.user)
+      .select()
+      .eq('email', email);
 
-  const email = session?.user?.email;
-  const { data } = await supabase
-    .from(TableNames.user)
-    .select()
-    .eq('email', email);
-
-  if (!data || data.length === 0) {
-    return NextResponse.redirect(`${req.nextUrl.origin}/register`);
+    if (!data || data.length === 0) {
+      return NextResponse.redirect(`${req.nextUrl.origin}/register`);
+    }
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/', '/register'],
+  matcher: ['/'],
 };
